584e00deb27879d467695d7a6399cf65675fb327
ztq <ztq@itcast.cn>
2021-01-09 20:40:44 +0800

    再次提交

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,72 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
-  })
-
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
-  })
-
-  // 从 layui 中获取 form 对象
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
-  form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
-        return '两次密码不一致！'
-      }
+// 注册div和登录div的按需切换
+// 点击去注册按钮
+$('#link_reg').on('click', function () {
+  $('.reg-box').show()
+  $('.login-box').hide()
+})
+// 点击去登录按钮
+$('#link_login').on('click', function () {
+  $('.reg-box').hide()
+  $('.login-box').show()
+})
+// 8.实现登录表单的验证
+// 自定义校验规则  1.密码必须6-12位   2.校验两次密码是否一致
+layui.form.verify({
+  pwd: [
+    /^[\S]{6,12}$/
+    , '密码必须6到12位，且不能出现空格'
+  ],
+  repwd: function () {
+    var pwdData = $('.reg-box [name=password]').val()
+    var repwdData = $('.reg-box [name=repassword]').val()
+    if (pwdData !== repwdData) {
+      return "两次密码不一致"
     }
-  })
-
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
+  }
+})
+// 9.提交注册的请求
+// 监听表单的提交事件
+$('#form_reg').on('submit', function (e) {
+  // 阻止表单的默认提交行为
+  e.preventDefault()
+  // 获取表单数据
+  var data = $(this).serialize()
+  // 使用ajax提交数据
+  $.ajax({
+    url: 'http://api-breakingnews-web.itheima.net/api/reguser',
+    method: 'post',
+    data: data,
+    success: function (res) {
       if (res.status !== 0) {
-        return layer.msg(res.message)
+        return layui.layer.msg(res.message)
+      } else {
+        // 10.注册结果的提交
+        layui.layer.msg("注册成功")
+        $('.reg-box').hide()
+        $('.login-box').show()
       }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
-    })
-  })
-
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
-    e.preventDefault()
-    $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('登录失败！')
-        }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
-      }
-    })
+    }
   })
 })
+// 11.登录的提交
+$('#form_login').on('submit', function (e) {
+  e.preventDefault();
+  var data = $(this).serialize()
+  $.ajax({
+    url: 'http://api-breakingnews-web.itheima.net/api/login',
+    method: 'post',
+    data: data,
+    success: function (res) {
+      console.log(res);
+      if (res.status !== 0) {
+        return ('登录失败')
+      } else {
+        localStorage.setItem('token', res.token)
+        location.href = 'index.html'
+      }
+    }
+
+  })
+
+})
+
